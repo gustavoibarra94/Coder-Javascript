e3b01e26c9691d0dbedc87a432d8532c0b72f6bc
user1 <gusanibarra.juegos@gmail.com>
2023-10-23 08:18:17 -0300

    Finalización de 1er ejercicio y revisión completa de toda la preentrega-2

--- preentrega-2.js
+++ preentrega-2.js
@@ -6,9 +6,14 @@ function PreEntrega2 (ejercicio) {
         if (ejercicio==1){
 
 
-            //const NombreUsuario = prompt `Por favor, ingrese su nombre y apellido`
+            // Bienvenida del usuario
 
-           // alert (`Bienvenido/a ${NombreUsuario} a la tienda online`)
+            const NombreUsuario = prompt `Por favor, ingrese su nombre y apellido`
+
+            alert (`Bienvenido/a ${NombreUsuario} a la tienda online`)
+
+
+            // Obtener Array de articulos seleccionados
 
             const ropaHombre = products.filter((productos) => productos.category === "men's clothing");
             console.log(ropaHombre)
@@ -21,12 +26,36 @@ function PreEntrega2 (ejercicio) {
 
             console.log(articulos)
 
-          const categoriasUnicas = []
 
-          console.log(categoriasUnicas)
+            // Obtener fechas y darle formato
+
+            const fechaActual = new Date ();
+
+            const fechaEntrega = new Date (fechaActual);
+
+            fechaEntrega.setDate(fechaActual.getDate()+2);
+
+            const formatoFecha = { year: 'numeric', month: 'long', day: 'numeric'};
+
+            //Declaro la fecha Actual con Formato por si es necesario usarla en un futuro, no se utilizará para este ejercicio
+
+            const fechaActualConFormato = fechaActual.toLocaleDateString('es-ES', formatoFecha);
+            const fechaEntregaConFormato = fechaEntrega.toLocaleDateString('es-ES', formatoFecha);
+
+            // Se calcula la diferencia de días en ms y días.
+
+            const diferenciaTiempo = fechaEntrega - fechaActual; 
+            const diferenciaDias = Math.ceil(diferenciaTiempo / (1000 * 60 * 60 * 24));
 
 
-          articulos.forEach((articulos) => {
+            // Comienzo
+
+            const categoriasUnicas = []
+
+            console.log(categoriasUnicas)
+
+
+             articulos.forEach((articulos) => {
 
             const aux = categoriasUnicas.find ((categoria) => categoria === articulos.category);
             if(aux === undefined){
@@ -56,7 +85,62 @@ function PreEntrega2 (ejercicio) {
 
           });
 
-         const productoACompar = prompt(`Los articulos disponibles para la compra son: ${listadoDeArt.join ( ` /// `)} por favor ingrese el nombre del articulo que desea comprar.`)
+
+
+          function busquedaProducto (nombreProducto) { 
+
+            
+
+            const productoElegido = articulos.find ((articulos) => articulos.title === nombreProducto)
+
+            console.log (productoElegido)
+            
+
+            if (productoElegido === undefined) { 
+
+                console.log (`error`)
+                nombreProducto = prompt (`El articulo ingresado no es correcto, por favor vuelva a ingresarlo, tené en cuenta que tiene que ser el nombre con mayúsculas, exacto y completo. Recuerde que los articulos disponibles para la compra son: ${listadoDeArt.join ( ` /// `)}`)
+                busquedaProducto (nombreProducto)
+            }
+
+            else { 
+
+                console.log (`ok`)
+                compraRealizara = confirm ( `Tu artítuclo elegido es ${productoElegido.title}, ${productoElegido.description} y su precio es ${productoElegido.price}. Desea realizar la compra?` )
+
+                if (compraRealizara === true) {
+
+                    alert (`Muchas gracias ${NombreUsuario} por la compra realizada. Su compra llegará dentro de ${diferenciaDias} días, el día ${fechaEntregaConFormato} `)
+
+                }
+
+                else {
+
+                    alert (`Compra Cancelada. Muchas gracias ${NombreUsuario} por utilizar nuestra tienda online, te esperamos nuevamente!`)
+
+                }
+
+            }
+
+
+
+         
+
+            console.log (nombreProducto)
+
+
+
+            }
+
+            
+
+         const nombreProducto = prompt(`Los articulos disponibles para la compra son: ${listadoDeArt.join ( ` /// `)} por favor ingrese el nombre del articulo que desea comprar.`)
+
+
+         busquedaProducto (nombreProducto)
+
+
+
 
          }
 
@@ -65,16 +149,19 @@ function PreEntrega2 (ejercicio) {
     //**************************************************************************************************************************************************************** */
         else if (ejercicio==2) {
 
+            // Bienvenida al usuario
+            
             const NombreUsuario = prompt `Por favor, ingrese su nombre y apellido`
 
             alert (`Bienvenido/a ${NombreUsuario} a la red social de Rick & Morty`)
 
 
+            // Randomize de Pj
+
+
             idPersonaje = Math.floor(Math.random() * 20) + 1;
 
             console.log (idPersonaje)
-
-
 
 
 
@@ -94,7 +181,7 @@ function PreEntrega2 (ejercicio) {
             data.sort ((a, b) => b.name.localeCompare(a.name));
 
 
-
+            // Primer valor de PJ de Z - A
 
            alert (`El último personaje en orden alfabético es ${data[0].name} `)
 
@@ -102,7 +189,7 @@ function PreEntrega2 (ejercicio) {
            
             function busquedaPersonaje (nombrePersonaje) { 
 
-            
+            // Elegir PJ y mostrar DATA
 
             const personajeElegido = data.find ((data) => data.name === nombrePersonaje)
 
